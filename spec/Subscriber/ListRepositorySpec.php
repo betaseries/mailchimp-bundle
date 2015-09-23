d719c54ef1b34d67126351f86b50af599227ef0a
tentacode <contact@gabrielpillet.com>
2015-09-23 17:08:50 +0200

    batchSubscribe test

--- spec/Subscriber/ListRepositorySpec.php
+++ spec/Subscriber/ListRepositorySpec.php
@@ -49,6 +49,124 @@ class ListRepositorySpec extends ObjectBehavior
         $this->subscribe(1337, $subscriber);
     }
 
+    function it_subscribes_several_subscribers($mailchimpLists, $logger)
+    {
+        $firstChunk = $this->getSubscriberChunk(500, 0);
+        $secondChunk = $this->getSubscriberChunk(500, 500);
+        $thirdChunk = $this->getSubscriberChunk(123, 1000);
+
+        $subscribers = array_merge($firstChunk, $secondChunk, $thirdChunk);
+
+        $options = ['mc_language' => 'fr'];
+
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($firstChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 500,
+            'update_count' => 0,
+            'error_count' => 0,
+            'success_count' => 0,
+            'errors' => []
+        ]);
+        
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($secondChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 250,
+            'update_count' => 250,
+            'error_count' => 0,
+            'success_count' => 0,
+            'errors' => []
+        ]);
+
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($thirdChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 100,
+            'update_count' => 22,
+            'error_count' => 1,
+            'success_count' => 0,
+            'errors' => [
+                ['email' => ['email' => 'foo@bar.com'], 'error' => 'Foo has errored.']
+            ]
+        ]);
+
+        $logger->info('850 subscribers added.')->shouldBeCalled();
+        $logger->info('272 subscribers updated.')->shouldBeCalled();
+        $logger->error('1 subscribers errored.')->shouldBeCalled();
+        $logger->error('Subscriber "foo@bar.com" has not been processed: "Foo has errored."')->shouldBeCalled();
+
+        $this->batchSubscribe(42, $subscribers, $options);
+    }
+
+    function it_unsubscribes_several_subscribers($mailchimpLists, $logger)
+    {
+        $firstChunk = $this->getSubscriberChunk(500, 0);
+        $secondChunk = $this->getSubscriberChunk(500, 500);
+        $thirdChunk = $this->getSubscriberChunk(123, 1000);
+
+        $subscribers = array_merge($firstChunk, $secondChunk, $thirdChunk);
+
+        $options = ['mc_language' => 'fr'];
+
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($firstChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 500,
+            'update_count' => 0,
+            'error_count' => 0,
+            'success_count' => 0,
+            'errors' => []
+        ]);
+        
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($secondChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 250,
+            'update_count' => 250,
+            'error_count' => 0,
+            'success_count' => 0,
+            'errors' => []
+        ]);
+
+        $mailchimpLists->batchSubscribe(
+            42,
+            $this->formatMailChimp($thirdChunk, $options),
+            false,
+            true
+        )->willReturn([
+            'add_count' => 100,
+            'update_count' => 22,
+            'error_count' => 1,
+            'success_count' => 0,
+            'errors' => [
+                ['email' => ['email' => 'foo@bar.com'], 'error' => 'Foo has errored.']
+            ]
+        ]);
+
+        $logger->info('850 subscribers added.')->shouldBeCalled();
+        $logger->info('272 subscribers updated.')->shouldBeCalled();
+        $logger->error('1 subscribers errored.')->shouldBeCalled();
+        $logger->error('Subscriber "foo@bar.com" has not been processed: "Foo has errored."')->shouldBeCalled();
+
+        $this->batchSubscribe(42, $subscribers, $options);
+    }
+
     function it_unsubscribe_a_subscriber($subscriber, $mailchimpLists)
     {
         $mailchimpLists->unsubscribe(
@@ -80,5 +198,25 @@ class ListRepositorySpec extends ObjectBehavior
         $mailchimpLists->getList(['list_name' => 'tutu'])->willReturn([
             'total' => 0
         ]);
+    }
+
+    protected function getSubscriberChunk($count, $offset)
+    {
+        $subscribers = [];
+        for ($i = $offset; $i < $offset + $count; $i++) {
+            $subscribers[] = new Subscriber(sprintf('email%s@example.org', $i));
+        }
+
+        return $subscribers;
+    }
+
+    protected function formatMailChimp(array $subscribers, array $options = [])
+    {
+        return array_map(function(Subscriber $subscriber) use ($options) {
+            return [
+                'email' => ['email' => $subscriber->getEmail()],
+                'merge_vars' => array_merge($options, $subscriber->getMergeTags())
+            ];
+        }, $subscribers);
     }
 }
